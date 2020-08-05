eac6877f18673576346c6702b638badc156f0f72
Wajdi-H <40223592+Wajdi-H@users.noreply.github.com>
2020-08-05 11:37:55 +0200

    frist commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { JoblistComponent } from './joblist/joblist.component';
+import { MenuComponent } from './menu/menu.component';
+const routes: Routes = [
+ 
+  { path: 'Home', component: HomeComponent },
+  { path: 'ListJobs', component: JoblistComponent },
+  { path: 'Menu', component: MenuComponent },
+  {path: '', redirectTo: 'Home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
